bb9aadb75b3a7b2c15a79773e57741841234d82a
vinceCoda <vcartillier3@gatech.edu>
2021-07-06 22:46:47 -0400

    adding mp3d dataloader

---
+++ mp3d/model_util_mp3d.py
@@ -0,0 +1,141 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+import numpy as np
+import sys
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(BASE_DIR)
+ROOT_DIR = os.path.dirname(BASE_DIR)
+
+class Mp3DDatasetConfig(object):
+    def __init__(self):
+        self.num_class = 15
+        self.num_heading_bin = 1
+        self.num_size_cluster = 15
+
+        self.type2class = {'chair': 0,
+                           'sofa': 1,
+                           'plant': 2,
+                           'bed': 3,
+                           'toilet': 4,
+                           'tv_monitor': 5,
+                           'shoe': 6,
+                           'dietary_supp': 7,
+                           'cartridge': 8,
+                           'doll_toy': 9,
+                           'puzzle_toy': 10,
+                           'lotion': 11,
+                           'vehicle_toy': 12,
+                           'figurine': 13,
+                           'bag': 14
+                          }
+
+        self.class2type = {self.type2class[t]:t for t in self.type2class}
+
+        # defined from Habitat y is up
+        self.type2mean_size = {
+            'chair': np.array([1.008, 0.755, 0.610]),
+            'sofa': np.array([2.310, 1.272, 0.855]),
+            'plant': np.array([1.341, 0.793, 0.555]),
+            'bed': np.array([2.260, 1.743, 0.980]),
+            'toilet': np.array([0.795, 0.671, 0.503]),
+            'tv_monitor': np.array([1.079, 0.722, 0.217]),
+
+            # NOTE: Attention for new assets dims where recorded with z up.
+            'shoe': np.array([0.166, 0.19, 0.287]),
+            'dietary_supp':  np.array([0.10, 0.137, 0.1]),
+            'cartridge':  np.array([0.116, 0.148, 0.077]),
+            'doll_toy':  np.array([0.265, 0.168, 0.235]),
+            'puzzle_toy':  np.array([0.288, 0.129, 0.242]),
+            'lotion':  np.array([0.11, 0.139, 0.074]),
+            'vehicle_toy':  np.array([0.174, 0.126, 0.183]),
+            'figurine': np.array([0.17, 0.178, 0.147]),
+            'bag':  np.array([0.31, 0.279, 0.231]),
+
+        }
+
+        #TODO convert sizes to z is up
+        for k in self.type2mean_size.keys():
+            v = self.type2mean_size[k]
+            self.type2mean_size[k] = np.array([v[0], v[2], v[1]])
+
+        self.mean_size_arr = np.zeros((self.num_size_cluster, 3))
+        for k,v in self.type2class.items():
+            self.mean_size_arr[v] = self.type2mean_size[k]
+
+        self.type_mean_size = {}
+        for i in range(self.num_size_cluster):
+            self.type_mean_size[self.class2type[i]] = self.mean_size_arr[i,:]
+
+    def angle2class(self, angle):
+        ''' Convert continuous angle to discrete class
+            [optinal] also small regression number from
+            class center angle to current angle.
+
+            angle is from 0-2pi (or -pi~pi), class center at 0, 1*(2pi/N), 2*(2pi/N) ...  (N-1)*(2pi/N)
+            return is class of int32 of 0,1,...,N-1 and a number such that
+                class*(2pi/N) + number = angle
+
+            NOT USED.
+        '''
+        angle = angle%(2*np.pi)
+        raise NotImplementedError
+
+
+
+    def class2angle(self, pred_cls, residual, to_label_format=True):
+        ''' Inverse function to angle2class.
+
+        As ScanNet only has axis-alined boxes so angles are always 0. '''
+        return 0
+
+    def size2class(self, size, type_name):
+        ''' Convert 3D box size (l,w,h) to size class and size residual '''
+        size_class = self.type2class[type_name]
+        size_residual = size - self.type_mean_size[type_name]
+        return size_class, size_residual
+
+    def class2size(self, pred_cls, residual):
+        ''' Inverse function to size2class '''
+        return self.mean_size_arr[pred_cls, :] + residual
+
+    def param2obb(self,
+                  center,
+                  heading_class,
+                  heading_residual,
+                  size_class,
+                  size_residual):
+
+        heading_angle = self.class2angle(heading_class, heading_residual)
+        box_size = self.class2size(int(size_class), size_residual)
+        obb = np.zeros((7,))
+        obb[0:3] = center
+        obb[3:6] = box_size
+        obb[6] = heading_angle*-1
+        return obb
+
+def rotate_aligned_boxes(input_boxes, rot_mat):
+    centers, lengths = input_boxes[:,0:3], input_boxes[:,3:6]
+    new_centers = np.dot(centers, np.transpose(rot_mat))
+
+    dx, dy = lengths[:,0]/2.0, lengths[:,1]/2.0
+    new_x = np.zeros((dx.shape[0], 4))
+    new_y = np.zeros((dx.shape[0], 4))
+
+    for i, crnr in enumerate([(-1,-1), (1, -1), (1, 1), (-1, 1)]):
+        crnrs = np.zeros((dx.shape[0], 3))
+        crnrs[:,0] = crnr[0]*dx
+        crnrs[:,1] = crnr[1]*dy
+        crnrs = np.dot(crnrs, np.transpose(rot_mat))
+        new_x[:,i] = crnrs[:,0]
+        new_y[:,i] = crnrs[:,1]
+
+
+    new_dx = 2.0*np.max(new_x, 1)
+    new_dy = 2.0*np.max(new_y, 1)
+    new_lengths = np.stack((new_dx, new_dy, lengths[:,2]), axis=1)
+
+    return np.concatenate([new_centers, new_lengths], axis=1)
---
+++ mp3d/mp3d_detection_dataset.py
@@ -0,0 +1,181 @@
+# coding: utf-8
+# Copyright (c) Facebook, Inc. and its affiliates.
+# 
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+""" Dataset for object bounding box regression.
+An axis aligned bounding box is parameterized by (cx,cy,cz) and (dx,dy,dz)
+where (cx,cy,cz) is the center point of the box, dx is the x-axis length of the box.
+"""
+import os
+import sys
+import numpy as np
+from torch.utils.data import Dataset
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+ROOT_DIR = os.path.dirname(BASE_DIR)
+sys.path.append(ROOT_DIR)
+sys.path.append(os.path.join(ROOT_DIR, 'utils'))
+import pc_util
+from model_util_mp3d import rotate_aligned_boxes
+
+from model_util_mp3d import Mp3DDatasetConfig
+DC = Mp3DDatasetConfig()
+#MAX_NUM_OBJ = 64
+#MEAN_COLOR_RGB = np.array([109.8, 97.2, 83.8])
+
+class ScannetDetectionDataset(Dataset):
+       
+    def __init__(self, 
+                 split_set='train', 
+                 num_points=20000,
+                 use_color=False, 
+                 use_height=False, 
+                 augment=False):
+
+        self.root_data_path = os.path.join(BASE_DIR, 'data/votenet_training_data/')
+
+        if split_set=='all':            
+            raise NotImplementedError
+        elif split_set in ['train', 'val', 'test']:
+            self.data_path = os.path.join(self.root_data_path, 
+                                          split_set, 
+                                          'votenet_inputs')
+            files = os.listdir(self.data_path)
+        else:
+            raise NotImplementedError
+        
+        self.num_points = num_points
+        self.use_color = use_color        
+        self.use_height = use_height
+        self.augment = augment
+       
+    def __len__(self):
+        return len(self.files)
+
+    def __getitem__(self, idx):
+        """
+        Returns a dict with following keys:
+            point_clouds: (N,3+C)
+            center_label: (MAX_NUM_OBJ,3) for GT box center XYZ
+            sem_cls_label: (MAX_NUM_OBJ,) semantic class index
+            angle_class_label: (MAX_NUM_OBJ,) with int values in 0,...,NUM_HEADING_BIN-1
+            angle_residual_label: (MAX_NUM_OBJ,)
+            size_classe_label: (MAX_NUM_OBJ,) with int values in 0,...,NUM_SIZE_CLUSTER
+            size_residual_label: (MAX_NUM_OBJ,3)
+            box_label_mask: (MAX_NUM_OBJ) as 0/1 with 1 indicating a unique box
+            point_votes: (N,3) with votes XYZ
+            point_votes_mask: (N,) with 0/1 with 1 indicating the point is in one of the object's OBB.
+            scan_idx: int scan index in scan_names list
+            pcl_color: unused
+        """
+        file = self.files[idx] 
+        
+        scan_name = file.split('.')[0]
+        env_uid, ep_uid = file.split('_')
+
+        h5file = h5py.File(os.path.join(self.data_path, file), 'r')
+        mesh_vertices = np.array(h5file['point_cloud'], dtype=np.float32)
+        instance_labels = np.array(h5file['instance_labels'], dtype=np.int32)
+        semantic_labels = np.array(h5file['semantic_labels'], dtype=np.int32)
+        instance_bboxes = np.array(h5file['bboxes'], dtype=np.float32)
+        h5file.close()
+
+        # convert PC to y is up.
+        mesh_vertices[...,[0,1,2]] = mesh_vertices[...,[0,2,1]]
+        
+        if not self.use_color:
+            point_cloud = mesh_vertices[:,0:3] # do not use color for now
+            pcl_color = mesh_vertices[:,3:6]
+        else:
+            point_cloud = mesh_vertices[:,0:6] 
+            point_cloud[:,3:] = (point_cloud[:,3:]-MEAN_COLOR_RGB)/256.0
+        
+        if self.use_height:
+            floor_height = np.percentile(point_cloud[:,2],0.99)
+            height = point_cloud[:,2] - floor_height
+            point_cloud = np.concatenate([point_cloud, np.expand_dims(height, 1)],1) 
+            
+        # ------------------------------- LABELS ------------------------------        
+        target_bboxes = np.zeros((MAX_NUM_OBJ, 6))
+        target_bboxes_mask = np.zeros((MAX_NUM_OBJ))    
+        angle_classes = np.zeros((MAX_NUM_OBJ,))
+        angle_residuals = np.zeros((MAX_NUM_OBJ,))
+        size_classes = np.zeros((MAX_NUM_OBJ,))
+        size_residuals = np.zeros((MAX_NUM_OBJ, 3))
+        
+        point_cloud, choices = pc_util.random_sampling(point_cloud, 
+                                                       self.num_points, 
+                                                       return_choices=True)        
+        instance_labels = instance_labels[choices]
+        semantic_labels = semantic_labels[choices]
+        
+        pcl_color = pcl_color[choices]
+        
+        target_bboxes_mask[0:instance_bboxes.shape[0]] = 1
+        target_bboxes[0:instance_bboxes.shape[0],:] = instance_bboxes[:,0:6]
+        
+        # ------------------------------- DATA AUGMENTATION ------------------------------        
+        if self.augment:
+            if np.random.random() > 0.5:
+                # Flipping along the YZ plane
+                point_cloud[:,0] = -1 * point_cloud[:,0]
+                target_bboxes[:,0] = -1 * target_bboxes[:,0]                
+                
+            if np.random.random() > 0.5:
+                # Flipping along the XZ plane
+                point_cloud[:,1] = -1 * point_cloud[:,1]
+                target_bboxes[:,1] = -1 * target_bboxes[:,1]                                
+            
+            # Rotation along up-axis/Z-axis
+            rot_angle = (np.random.random()*np.pi/18) - np.pi/36 # -5 ~ +5 degree
+            rot_mat = pc_util.rotz(rot_angle)
+            point_cloud[:,0:3] = np.dot(point_cloud[:,0:3], np.transpose(rot_mat))
+            target_bboxes = rotate_aligned_boxes(target_bboxes, rot_mat)
+
+        # compute votes *AFTER* augmentation
+        # generate votes
+        # Note: since there's no map between bbox instance labels and
+        # pc instance_labels (it had been filtered 
+        # in the data preparation step) we'll compute the instance bbox
+        # from the points sharing the same instance label. 
+        point_votes = np.zeros([self.num_points, 3])
+        point_votes_mask = np.zeros(self.num_points)
+        for i_instance in np.unique(instance_labels):            
+            # find all points belong to that instance
+            ind = np.where(instance_labels == i_instance)[0]
+            # find the semantic label            
+            #TODO: change classe labels
+            if not (semantic_labels[ind[0]] == -1):
+                x = point_cloud[ind,:3]
+                center = 0.5*(x.min(0) + x.max(0))
+                point_votes[ind, :] = center - x
+                point_votes_mask[ind] = 1.0
+        point_votes = np.tile(point_votes, (1, 3)) # make 3 votes identical 
+        
+        # NOTE: set size class as semantic class. Consider use size2class.
+        size_classes[0:instance_bboxes.shape[0]] = instance_bboxes[:,-1]
+        size_residuals[0:instance_bboxes.shape[0], :] = \
+            target_bboxes[0:instance_bboxes.shape[0], 3:6] - DC.mean_size_arr[instance_bboxes[:,-1],:]
+        
+        #TODO: update angle_classes + residuals
+        angle_residuals[0:instance_bboxes.shape[0]] = instance_bboxes[:,-2]
+
+        ret_dict = {}
+        ret_dict['point_clouds'] = point_cloud.astype(np.float32)
+        ret_dict['center_label'] = target_bboxes.astype(np.float32)[:,0:3]
+        ret_dict['heading_class_label'] = angle_classes.astype(np.int64)
+        ret_dict['heading_residual_label'] = angle_residuals.astype(np.float32)
+        ret_dict['size_class_label'] = size_classes.astype(np.int64)
+        ret_dict['size_residual_label'] = size_residuals.astype(np.float32)
+        target_bboxes_semcls = np.zeros((MAX_NUM_OBJ))                                
+        target_bboxes_semcls[0:instance_bboxes.shape[0]] = instance_bboxes[:,-1]                
+        ret_dict['sem_cls_label'] = target_bboxes_semcls.astype(np.int64)
+        ret_dict['box_label_mask'] = target_bboxes_mask.astype(np.float32)
+        ret_dict['vote_label'] = point_votes.astype(np.float32)
+        ret_dict['vote_label_mask'] = point_votes_mask.astype(np.int64)
+        ret_dict['scan_idx'] = np.array(idx).astype(np.int64)
+        #ret_dict['pcl_color'] = pcl_color
+        return ret_dict
+        
+
